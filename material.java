1b7aa12b5bce6a05605be61f195a27d67cf623c1
Malachy O'Connor School <110703067+OconnorMalachy@users.noreply.github.com>
2024-12-04 05:57:02 -0500

    Delete material.java

--- material.java
+++
@@ -1,9 +0,0 @@
-public abstract class material{
-    public color emitted(double u, double v, vec3 p){
-        return new color(0,0,0);
-    }
-    public boolean scatter(ray rIn, hitRecord rec, color attenuation, ray scattered){
-        return false;
-    }
-
-}
